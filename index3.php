0c8a830b8886f6a3d9bfff00133bc7a60d1c73f0
DanielVial15 <danielvial@hotmail.com>
2022-05-05 16:08:57 -0300

    Inserido um novo index.thml e um CSS

---
+++ index3.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Accordion - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
+</head>
+<body>
+ 
+<div id="accordion">
+  <h3>Quem sou eu</h3>
+  <div>
+    <p>
+    Sou o Daniel, tenho 37 anos e pretendo me tornar um desenvolvedor full-stack.
+    </p>
+  </div>
+  <h3>De onde sou</h3>
+  <div>
+    <p>
+    Sou morador de Belo Horizonte, nascido e criado.
+    </p>
+  </div>
+  <h3>Formação</h3>
+  <div>
+    <p>
+    Tenho uma formação de Ciência da Computação. E também tenho as seguintes formações abaixo:
+    </p>
+    <ul>
+      <li>Ensino médio</li>
+      <li>Técnico em eletrônica</li>
+    </ul>
+  </div>
+</div>
+ 
+ 
+</body>
+</html>
